e51da01e953797ba56b0fceca7e4f634d59ea0ca
Noah Day <sheltielover432@gmail.com>
2020-04-30 19:46:40 -0400

    ECS Part 1

---
+++ Flyweight_Pattern/Assets/Unity ECS/Warthog/SpawnCar.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnCar : MonoBehaviour
+{
+    public GameObject car, camera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.P))
+        {
+            Vector3 pos = new Vector3(10, 10, 10);
+            GameObject c = Instantiate(car, pos, Quaternion.identity);
+            camera.GetComponent<SmoothFollow>().target = c.transform;
+        }
+    }
+}
