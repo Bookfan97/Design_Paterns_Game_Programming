983703412c1d313353a5a8435ab76f2387397d2f
Noah Day <sheltielover432@gmail.com>
2020-04-29 21:19:22 -0400

    SO Part 1

---
+++ Flyweight_Pattern/Assets/Scriptable Objects/Scripts/PlantData.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "plantData", menuName ="Plant Data", order = 51)]
+public class PlantData : ScriptableObject
+{
+public enum THREAT { None, Low, Moderate, High, Midnight}
+    [SerializeField]
+    private string plantName;
+    [SerializeField]
+    private THREAT plantThreat;
+    [SerializeField]
+    private Texture icon;
+}
